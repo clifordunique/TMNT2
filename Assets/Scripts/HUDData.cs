0c42cae93e0b74b6f57c8e1391801f59c0bfa026
Michael Tesija <mtesija@umich.edu>
2014-01-16 16:26:26 -0500

    Small Changes

---
+++ Assets/Scripts/HUDData.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUDData : MonoBehaviour
+{
+	private string playerName = "";
+	public int pts = 0;
+
+	void Start()
+	{
+		//Get Player Name
+	}
+
+	void Update()
+	{
+		//draw playerName
+		//draw pts
+		//Get life
+		//draw life
+		//Get rest
+		//draw rest
+	}
+}
