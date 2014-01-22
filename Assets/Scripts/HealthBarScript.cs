a50583be11e7511704c6a4c3e16c31261a024bec
Michael Tesija <mtesija@umich.edu>
2014-01-22 03:02:33 -0500

    HealthBar, door animations, and some scripting

---
+++ Assets/Scripts/HealthBarScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBarScript : MonoBehaviour {
+	private Animator animator;
+
+	void Start()
+	{
+		animator = this.GetComponent<Animator>();
+	}
+
+	void Update() 
+	{
+		GameObject player = GameObject.Find("Player");
+		PlayerScript playerScript = player.GetComponent<PlayerScript>();
+		animator.SetInteger("life", playerScript.life);
+	}
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public int life = 12;
+	public int rest = 2;
+	public int pts = 0;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
