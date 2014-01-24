f0c3083081b600b6e686dfcf1845cf149cdd147d
Michael Tesija <mtesija@umich.edu>
2014-01-24 02:09:49 -0500

    Ground shadow while jumping

---
+++ Assets/Scripts/ShadowScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShadowScript : MonoBehaviour {
+	private SpriteRenderer renderer;
+
+	void Start() 
+	{
+		renderer = this.GetComponent<SpriteRenderer>();
+	}
+
+	void Update() 
+	{
+		GameObject player = GameObject.Find("Player");
+		PlayerScript playerScript = player.GetComponent<PlayerScript>();
+
+		if(playerScript.jumpPos == 0)
+		{
+			this.renderer.enabled = false;
+		}
+		else
+		{
+			this.renderer.enabled = true;
+		}
+
+		Vector3 position = player.transform.position;
+		position.y = position.z;
+		this.transform.position = position;
+	}
+}
