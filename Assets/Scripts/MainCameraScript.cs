ffd3ffee25185a0e59408b127d545b8ed9744027
Michael Tesija <mtesija@umich.edu>
2014-01-26 04:16:44 -0500

    Screen scroll, began implementing attacks

---
+++ Assets/Scripts/MainCameraScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCameraScript : MonoBehaviour {
+
+	void Update()
+	{
+		GameObject player = GameObject.Find("Player");
+		float dist = (transform.position - Camera.main.transform.position).z;
+		float threshold = Camera.main.ViewportToWorldPoint(new Vector3(.6f,0,dist)).x;
+		if(player.transform.position.x > threshold)
+		{
+			PlayerScript playerScript = player.GetComponent<PlayerScript>();
+			if(playerScript.deltaX > 0)
+			{
+				Vector3 position = transform.position;
+				position.x += playerScript.deltaX;
+				transform.position = position;
+			}
+		}
+	}
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -8,13 +8,19 @@ public class PlayerScript : MonoBehaviour {
 	public int pts = 0;
 	public string turtleName = "RAPH";
 
-
+	private bool attacking = false;
 
 	public float deltaX = 0;
+	private float deltaY = 0;
+	private float deltaJump = 0;
+
 	public float yPos = 0;
+
 	public float jumpPos = 0;
 	private float jumpVelocity = 0;
 	private float jumpAccel = -10f;
+	
+	private float jumpKickVelocity = 0;
 
 	private Animator animator;
 	
@@ -26,36 +32,50 @@ public class PlayerScript : MonoBehaviour {
 
 	void Update() 
 	{
-		//Didnt use getaxis because it has acceleration
+		if(attacking)
+		{
+			if(jumpPos == 0)
+			{
+				attacking = false;
+			}
+		}
+
 		deltaX = 0;
+		deltaY = 0;
+		deltaJump = 0;
+
+		if(!attacking)
+		{
+			playerInput();
+		}
+
+		playerMovement();
+
+		playerAnimations();
+	}
+
+	void playerInput()
+	{
+		//Didnt use getaxis because it has acceleration
 
 		if(Input.GetKey(KeyCode.RightArrow))
 		{
 			deltaX += .8f;
 		}
+
 		if(Input.GetKey(KeyCode.LeftArrow))
 		{
 			deltaX += -.8f;
 		}
 
-		float deltaY = 0;
-
 		if(Input.GetKey(KeyCode.UpArrow))
 		{
 			deltaY += .7f;
 		}
+
 		if(Input.GetKey(KeyCode.DownArrow))
 		{
 			deltaY += -.7f;
-		}
-
-
-		float deltaJump = 0;
-
-		if(jumpPos > 0)
-		{
-			jumpVelocity += jumpAccel * Time.deltaTime;
-			deltaJump += jumpVelocity;
 		}
 
 		if(Input.GetKeyDown(KeyCode.F) && jumpPos == 0)
@@ -64,18 +84,46 @@ public class PlayerScript : MonoBehaviour {
 			deltaJump += jumpVelocity;
 		}
 
-		if(jumpPos < 0)
+		if(Input.GetKeyDown(KeyCode.D))
 		{
+			attacking = true;
+
+			if(jumpVelocity < 3.6f)
+			{
+				jumpKickVelocity = 3f;
+				jumpVelocity = -.8f;
+
+				if(deltaX != 0)
+				{
+					jumpKickVelocity *= Mathf.Sign(deltaX);
+				}
+				else
+				{
+					jumpKickVelocity *= transform.localScale.x;
+				}
+			}
+		}
+	}
+
+	void playerMovement()
+	{
+		if(jumpPos > 0)
+		{
+			jumpVelocity += jumpAccel * Time.deltaTime;
+			deltaJump += jumpVelocity;
+		}
+		
+		if(jumpPos <= 0)
+		{
+			jumpKickVelocity = 0;
 			jumpPos = 0;
 		}
-
-		jumpPos += deltaJump;
 		
-		float x = transform.position.x + deltaX;
+		jumpPos += deltaJump;
 
 		//Using Z axis for Isometric effect
 		float z = yPos + deltaY;
-
+		
 		if(z > 105)
 		{
 			z = 105;
@@ -84,55 +132,85 @@ public class PlayerScript : MonoBehaviour {
 		{
 			z = 15;
 		}
-
+		
 		yPos = z;
-
+		
 		float y = z + jumpPos + deltaJump;
 
-		transform.position = new Vector3(x, y, z);
+		float x = transform.position.x + deltaX + jumpKickVelocity;
+		
+		float dist = (transform.position - Camera.main.transform.position).z;
+		float leftBorder = Camera.main.ViewportToWorldPoint(new Vector3(.0625f,0,dist)).x;
+		float rightBorder = Camera.main.ViewportToWorldPoint(new Vector3(.9125f,0,dist)).x;
 
-
-
-
-
-
-
-
-
-
-		//Flip sprite left/right
-		if (deltaX < 0 && transform.localScale.x > 0)
-			transform.localScale = new Vector3(-1f, 1f, 1f);
-		else if (deltaX > 0 && transform.localScale.x < 0)
-			transform.localScale = new Vector3(1f, 1f, 1f);
-
-		//All animations are handled here
-		if(jumpPos == 0)
+		if(x < leftBorder)
 		{
-			if(deltaX != 0 || deltaY != 0)
+			x = leftBorder;
+		}
+		else if(x > rightBorder)
+		{
+			x = rightBorder;
+		}
+		
+		transform.position = new Vector3(x, y, z);
+	}
+
+	void playerAnimations()
+	{		
+		if(attacking)
+		{
+			if(jumpPos == 0)
 			{
-				animator.Play("RaphaelWalk");
+				animator.Play("RaphaelAttack2");
 			}
 			else
 			{
-				animator.Play("RaphaelIdle");
+				//Flip sprite left/right
+				if (deltaX < 0 && transform.localScale.x > 0)
+					transform.localScale = new Vector3(-1f, 1f, 1f);
+				else if (deltaX > 0 && transform.localScale.x < 0)
+					transform.localScale = new Vector3(1f, 1f, 1f);
+
+				if(jumpVelocity < 3.6f)
+				{
+					animator.Play("RaphaelJumpKick");
+				}
+				else
+				{
+					//Special Attack
+				}
 			}
 		}
 		else
 		{
-
-
-			if(jumpVelocity < 3.6f)
+			if(jumpPos == 0)
 			{
-				animator.Play("RaphaelJumpSpin");
+				//Flip sprite left/right
+				if (deltaX < 0 && transform.localScale.x > 0)
+					transform.localScale = new Vector3(-1f, 1f, 1f);
+				else if (deltaX > 0 && transform.localScale.x < 0)
+					transform.localScale = new Vector3(1f, 1f, 1f);
+
+				if(deltaX != 0 || deltaY != 0)
+				{
+					animator.Play("RaphaelWalk");
+				}
+				else
+				{
+					animator.Play("RaphaelIdle");
+				}
 			}
 			else
 			{
-				animator.Play("RaphaelJump");
+				if(jumpVelocity < 3.6f)
+				{
+					animator.Play("RaphaelJumpSpin");
+				}
+				else
+				{
+					animator.Play("RaphaelJump");
+				}
 			}
-
-
-
 		}
 	}
 }
