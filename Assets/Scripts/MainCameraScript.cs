37e3fb9d5d83713dad057ae8f5a0489a2870839e
Michael Tesija <mtesija@umich.edu>
2014-01-26 20:38:21 -0500

    Camera stops scrolling at end of level

--- Assets/Scripts/MainCameraScript.cs
+++ Assets/Scripts/MainCameraScript.cs
@@ -2,20 +2,29 @@
 using System.Collections;
 
 public class MainCameraScript : MonoBehaviour {
+	public bool stopMovement = false;
 
 	void Update()
 	{
-		GameObject player = GameObject.Find("Player");
-		float dist = (transform.position - Camera.main.transform.position).z;
-		float threshold = Camera.main.ViewportToWorldPoint(new Vector3(.6f,0,dist)).x;
-		if(player.transform.position.x > threshold)
+		if(this.transform.position.x > 1792 - (256 / 2))
 		{
-			PlayerScript playerScript = player.GetComponent<PlayerScript>();
-			if(playerScript.deltaX > 0)
+			stopMovement = true;
+		}
+		
+		if(!stopMovement)
+		{
+			GameObject player = GameObject.Find("Player");
+			float dist = (transform.position - Camera.main.transform.position).z;
+			float threshold = Camera.main.ViewportToWorldPoint(new Vector3(.6f, 0, dist)).x;
+			if(player.transform.position.x > threshold)
 			{
-				Vector3 position = transform.position;
-				position.x += playerScript.deltaX;
-				transform.position = position;
+				PlayerScript playerScript = player.GetComponent<PlayerScript>();
+				if(playerScript.deltaX > 0)
+				{
+					Vector3 position = transform.position;
+					position.x += playerScript.deltaX;
+					transform.position = position;
+				}
 			}
 		}
 	}
