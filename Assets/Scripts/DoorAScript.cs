6dd4b4a93105c623e793f2ea2006518e93d5d41a
Michael Tesija <mtesija@umich.edu>
2014-01-27 16:20:31 -0500

    DoorA works properly DoorB almost set up

---
+++ Assets/Scripts/DoorAScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorAScript : MonoBehaviour {
+	Animator animator;
+
+	// Use this for initialization
+	void Start() 
+	{
+		animator = this.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update() 
+	{
+		if(animator.GetCurrentAnimatorStateInfo(0).IsName("DoorAEnd"))
+		{
+			Destroy(this.gameObject);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D coll)
+	{
+		if(coll.gameObject.name == "Player")
+		{
+			animator.Play("DoorAAnimation");
+		}
+	}
+}
---
+++ Assets/Scripts/DoorBScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorBScript : MonoBehaviour {
+	Animator animator;
+	
+	// Use this for initialization
+	void Start() 
+	{
+		animator = this.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update() 
+	{
+		if(animator.GetCurrentAnimatorStateInfo(0).IsName("DoorBEnd"))
+		{
+			Destroy(this.gameObject);
+		}
+	}
+	
+	void OnTriggerEnter2D(Collider2D coll)
+	{
+		if(coll.gameObject.name == "Player")
+		{
+			animator.Play("DoorBAnimation");
+		}
+	}
+}
