a8c93181a0a54112ffad6429afccbe354e18e77d
niftybottle <niftybottle@gmail.com>
2014-02-06 00:30:12 -0500

    Added second screen of enemies
    Note - potential bug: will think all enemies for this screen are dead if
    kill all enemies spawned before next spawn (ie, if you kill first enemy
    before 2nd spawns, the second will not be the attacker)

--- Assets/Scripts/S1Spawner.cs
+++ Assets/Scripts/S1Spawner.cs
@@ -10,20 +10,32 @@ public class S1Spawner : MonoBehaviour {
 	public int goTime = 0;
 	public MainCameraScript cams;
 	public int attacker;
-
+	public float screenstop = 320f;
+	public float right;
+	public float left;
 
 	// Use this for initialization
 	void Start () {
 		PattCol = GameObject.Find("PlayerAttackCollider").GetComponent<BoxCollider2D>();
 		PCol = GameObject.Find("Player").GetComponent<BoxCollider2D>();
 		cams = GameObject.Find("Main Camera").GetComponent<MainCameraScript>();
+		pfs = new GameObject[4];
+		right = screenstop + 32f;
+		left = screenstop - (32f + 256f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(go){
 			if(goTime == 0){
-
+				spawn(1, new Vector3(right, 105f, 105f), 2);
+			}else if(goTime == 50){
+				spawn(2, new Vector3(left, 80f, 80f), 1);
+			}else if(goTime == 100){
+				spawn (3, new Vector3(left, 40f, 40f), 1);
+			}else if(goTime == 150){
+				spawn(1, new Vector3(right, 105f, 105f), 1);
+				go = false;
 			}
 			goTime++;
 		}
@@ -33,10 +45,10 @@ public class S1Spawner : MonoBehaviour {
 			go = true;
 		}
 	}
-	void spawn(int num, Vector3 vec){
+	void spawn(int num, Vector3 vec, int source){
 		pfs[num] = Instantiate(PurpleFS, vec, Quaternion.identity) as GameObject;
 		PurpleFSScript pfsscript = pfs[num].GetComponent<PurpleFSScript>();
-		pfsscript.source = 1;
+		pfsscript.source = source;
 		if (num == 1){
 			pfsscript.attacker = true;
 			attacker = 1;
