10425e63094405c682c43c4ef8e1f5dd6ca7c058
niftybottle <niftybottle@gmail.com>
2014-02-07 16:16:30 -0500

    Begun work on star throwing foot soldier

--- Assets/Scripts/StarFSScript.cs
+++ Assets/Scripts/StarFSScript.cs
@@ -1,42 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PurpleFSScript : MonoBehaviour {
+public class StarFSScript : MonoBehaviour {
 	public int life = 2;
 	public int gothit = 0;
 	
 	public bool attacking = false;
 	private static float bodylength = 22;
 	//Probably should fix that to be updated if it changes
-	private float jumpdist = bodylength*3f;
+	private float throwdist = bodylength*3f;
 	private float punchdist = 34;
 	public bool attacker = false;
 	private float rdir = 0;
 	private float rtime = 0;
 	public Component spawner;
 	public int num;
-	public bool jumped = false;
-
+	
 	
 	
 	public float deltaX = 0;
 	private float deltaY = 0;
-	private float deltaJump = 0;
 	
 	public float yPos = 0;
-	
-	public float jumpPos = 0;
-	private float jumpVelocity = 0;
-	private float jumpAccel = -10f;
-	
-	public float jumpKickVelocity = 0;
 
+	
 	private BoxCollider2D attackCollider;
-	private BoxCollider2D jumpColl;
 	private PlayerScript player;
-
+	
 	private int entering = 5;
-
+	
 	//1 from left, 2 right, 3 door
 	public int source;
 	
@@ -47,10 +39,7 @@ public class PurpleFSScript : MonoBehaviour {
 	void Start () {
 		animator = this.GetComponent<Animator>();
 		yPos = transform.position.y;
-		//Entrance animation if applicable?
-		//attackCollider = GameObject.Find("PFSAttColl").GetComponent<BoxCollider2D>();
-		attackCollider = this.transform.FindChild("PFSAttColl").GetComponent<BoxCollider2D>();
-		jumpColl = this.transform.FindChild("PFSJumpColl").GetComponent<BoxCollider2D>();
+		attackCollider = this.transform.FindChild("SFSAttColl").GetComponent<BoxCollider2D>();
 		player = GameObject.Find("Player").GetComponent<PlayerScript>();
 	}
 	
@@ -64,21 +53,17 @@ public class PurpleFSScript : MonoBehaviour {
 		float xdist = Mathf.Abs(playerx-x);
 		float ydist = Mathf.Abs(playery-yPos);
 		//animator.SetBool ("Walking", false);
-
+		
 		
 		deltaX = 0;
 		deltaY = 0;
-		deltaJump = 0;
-		/*if(jumpPos == 0){
-			animator.SetBool ("Jumping", false);
-		}*/
 		
 		if(!attacking && hitCooldown == 0)
 		{
 			if(xdist <= punchdist && ydist <= 5){
 				punch();
 			}else if(attacker){
-				PurpleFSInput(x, playerx, playery, xdist, ydist);
+				StarFSInput(x, playerx, playery, xdist, ydist);
 			}else if(rtime == 0) {
 				wander();
 			}else{
@@ -94,43 +79,23 @@ public class PurpleFSScript : MonoBehaviour {
 		}
 		if(attacking && hitCooldown == 0)
 		{
-			if (jumpPos == 0 && animator.GetCurrentAnimatorStateInfo(0).IsName("PunchEnd")){
+			if (animator.GetCurrentAnimatorStateInfo(0).IsName("PunchEnd")){
 				attacking = false;
 				animator.SetBool ("Attacking", false);
 				attackCollider.enabled = false;
 			}
 		}
 		
-		PurpleFSMovement();
+		StarFSMovement();
 		if(hitCooldown > 0){
 			hitCooldown--;
 			if(hitCooldown == 0){
 				animator.SetBool ("Hit", false);
 			}
 		}
-		
-		//PurpleFSAnimations();
 	}
-	void PurpleFSMovement()
+	void StarFSMovement()
 	{
-		if(jumpPos > 0)
-		{
-			jumpVelocity += jumpAccel * Time.deltaTime;
-			deltaJump += jumpVelocity;
-			animator.SetBool ("Jumping", true);
-			jumpColl.enabled = true;
-		}
-		
-		if(jumpPos <= 0)
-		{
-			jumpKickVelocity = 0;
-			jumpPos = 0;
-			animator.SetBool ("Jumping", false);
-			jumpColl.enabled = false;
-			attacking = false;
-		}
-		
-		jumpPos += deltaJump;
 		
 		//Using Z axis for Isometric effect
 		float z = yPos + deltaY;
@@ -146,22 +111,22 @@ public class PurpleFSScript : MonoBehaviour {
 		z = Mathf.Round(z*10f)/10f;
 		yPos = z;
 		
-		float y = z + jumpPos + deltaJump;
+		float y = z;
 		
-		float x = transform.position.x + deltaX + jumpKickVelocity;
+		float x = transform.position.x + deltaX;
 		//y = Mathf.Round(y*10f)/10f;
 		x = Mathf.Round(x*10f)/10f;
 		if (deltaX < 0 && transform.localScale.x > 0)
 			transform.localScale = new Vector3(-1f, 1f, 1f);
 		else if (deltaX > 0 && transform.localScale.x < 0)
 			transform.localScale = new Vector3(1f, 1f, 1f);
-		if ((deltaX != 0 || deltaY != 0) && jumpPos == 0){
+		if ((deltaX != 0 || deltaY != 0)){
 			animator.SetBool ("Walking", true);
 		}
 		
 		transform.position = new Vector3(x, y, z);
 	}
-	void PurpleFSInput(float x, float playerx, float playery, float xdist, float ydist)
+	void StarFSInput(float x, float playerx, float playery, float xdist, float ydist)
 	{
 		if(playerx > (x + punchdist))
 		{
@@ -173,63 +138,44 @@ public class PurpleFSScript : MonoBehaviour {
 			deltaX += -.7f;
 		}
 		
-		if(jumpPos == 0 && ydist >= (xdist-punchdist) && playery > yPos+5)
+		if(ydist >= (xdist-punchdist) && playery > yPos+5)
 		{
 			deltaY += .6f;
 		}
 		
-		if(jumpPos == 0 && ydist >= (xdist-punchdist) && playery < yPos - 5)
+		if(ydist >= (xdist-punchdist) && playery < yPos - 5)
 		{
 			deltaY += -.6f;
 		}
 		
-		if((xdist < jumpdist) && (xdist > punchdist + .7f) && jumpPos == 0)
+		if((xdist < throwdist) && (xdist > punchdist + .7f))
 		{
-			float jumpchance = Random.value;
-			if(jumpchance <= .01f)
-			{
-				animator.SetBool ("Walking", false);
-				jumpColl.enabled = true;
-				jumpVelocity = 5.7f;
-				deltaJump += jumpVelocity;
-				jumpKickVelocity = 3f;
-				//jumpKickVelocity *= Mathf.Sign(playerx-x);
-				if(deltaX != 0)
-				{
-					jumpKickVelocity *= Mathf.Sign(deltaX);
-				}
-				else
-				{
-					jumpKickVelocity *= transform.localScale.x;
-				}
-				animator.SetBool("Jumping", true);
-				jumped = true;
-				attacking = true;
-			}
+//			float jumpchance = Random.value;
+//			if(jumpchance <= .01f)
+//			{
+//				animator.SetBool ("Walking", false);
+//				jumpVelocity = 5.7f;
+//				deltaJump += jumpVelocity;
+//				//attacking = true;
+//				jumpKickVelocity = 3f;
+//				//jumpKickVelocity *= Mathf.Sign(playerx-x);
+//				if(deltaX != 0)
+//				{
+//					jumpKickVelocity *= Mathf.Sign(deltaX);
+//				}
+//				else
+//				{
+//					jumpKickVelocity *= transform.localScale.x;
+//				}
+//				animator.SetBool("Jumping", true);
+//				jumped = true;
+//				attacking = true;
+//			}
 		}
-		/*if(Input.GetKeyDown(KeyCode.D))
-		{
-			attacking = true;
-			
-			if(jumpVelocity < 3.6f)
-			{
-				jumpKickVelocity = 3f;
-				jumpVelocity = -.8f;
-				
-				if(deltaX != 0)
-				{
-					jumpKickVelocity *= Mathf.Sign(deltaX);
-				}
-				else
-				{
-					jumpKickVelocity *= transform.localScale.x;
-				}
-			}
-		}*/
 	}
 	void wander(){
 		float dir = Random.value;
-
+		
 		if (dir <= .5){
 			rdir = 1;
 			deltaX += .7f;
@@ -249,12 +195,12 @@ public class PurpleFSScript : MonoBehaviour {
 		{
 			return;
 		}
-
+		
 		gothit++;
 		if(other.gameObject.name == "PlayerAttackCollider"){
 			OnHit();
 		}
-
+		
 	}
 	void OnHit(){
 		animator.SetBool ("Walking", false);
@@ -266,7 +212,7 @@ public class PurpleFSScript : MonoBehaviour {
 		{
 			life -= 1;
 		}
-
+		
 		if(life <= 0)
 		{
 			die();
